9e518149b5584616313db7fdc181d60c9c978a74
Jae Hyun Yoo <fkkcloud@gmail.com>
2016-02-14 14:42:19 -0800

    device pixel ratio test

--- TOB/js/boot.js
+++ TOB/js/boot.js
@@ -5,6 +5,27 @@ var Boot = function(game){
 Boot.prototype = {
 	init: function(){
 
+		this.input.maxPointers = 1;
+        this.stage.disableVisibilityChange = true;
+
+        if (this.game.device.desktop)
+        {
+            this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+            this.scale.setMinMax(480, 260, 1024, 768);
+            this.scale.pageAlignHorizontally = true;
+            this.scale.pageAlignVertically = true;
+        }
+        else
+        {
+            this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+            this.scale.setMinMax(480, 260, 1024, 768);
+            this.scale.pageAlignHorizontally = true;
+            this.scale.pageAlignVertically = true;
+            this.scale.forceOrientation(true, false);
+            this.scale.setResizeCallback(this.gameResized, this);
+            this.scale.enterIncorrectOrientation.add(this.enterIncorrectOrientation, this);
+            this.scale.leaveIncorrectOrientation.add(this.leaveIncorrectOrientation, this);
+        }
 	},
 
 	preload: function(){
@@ -13,7 +34,7 @@ Boot.prototype = {
 	
   	create: function(){
 		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-		//this.scale.setScreenSize(true);
+		this.scale.setScreenSize(true);
 		this.game.state.start("Preload");
 	}
 }
